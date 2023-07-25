736ab969d5bac5cf0b26198e98d20aeb9477a262
grimaldo.soto <Grimaldo.SOTO@icloud.com>
2023-07-24 19:02:23 -0500

    Ciclo de vida de los componentes

--- src/app/modules/task/page/task-page.component.ts
+++ src/app/modules/task/page/task-page.component.ts
@@ -1,25 +1,135 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-task-page',
   templateUrl: './task-page.component.html',
   styleUrls: ['./task-page.component.css'],
 })
-export class TaskPageComponent {
+export class TaskPageComponent implements OnInit {
 
-  listUser:Array<{id:string, name:string}> =[
+  groups: Array<any> = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.groups = [
+      {
+        label: 'Nuevos',
+        color: 'tomato',
+        list: [
+          {
+            order: 'Como instalar Angular',
+            items: [
+              {
+                key: 'price',
+                value: 152,
+              },
+              {
+                key: 'time',
+                value: 152,
+              },
+              {
+                key: 'author',
+                value: {
+                  name: 'Grimaldo SOTO',
+                  avatar:
+                    'https://avatars.githubusercontent.com/u/118828231?v=4"',
+                },
+              },
+            ],
+          },
+          {
+            order: 'Como instalar Angular',
+            items: [
+              {
+                key: 'price',
+                value: 152,
+              },
+              {
+                key: 'time',
+                value: 152,
+              },
+              {
+                key: 'author',
+                value: {
+                  name: 'Grimaldo SOTO',
+                  avatar:
+                    'https://avatars.githubusercontent.com/u/118828231?v=4"',
+                },
+              },
+            ],
+          },
+        ],
+      },
+      {
+        label: 'in progress',
+        color: 'tomato',
+        list: [
+          {
+            order: 'Como instalar Angular',
+            items: [
+              {
+                key: 'price',
+                value: 152,
+              },
+              {
+                key: 'time',
+                value: 152,
+              },
+              {
+                key: 'author',
+                value: {
+                  name: 'Grimaldo SOTO',
+                  avatar:
+                    'https://avatars.githubusercontent.com/u/118828231?v=4"',
+                },
+              },
+            ],
+          },
+        ],
+      },
+      {
+        label: 'finish',
+        color: 'tomato',
+        list: [
+          {
+            order: 'Como instalar Angular',
+            items: [
+              {
+                key: 'price',
+                value: 152,
+              },
+              {
+                key: 'time',
+                value: 152,
+              },
+              {
+                key: 'author',
+                value: {
+                  name: 'Grimaldo SOTO',
+                  avatar:
+                    'https://avatars.githubusercontent.com/u/118828231?v=4"',
+                },
+              },
+            ],
+          },
+        ],
+      },
+    ];
+  }
+
+  listUser: Array<{ id: string; name: string }> = [
     {
-      id:'1',
-      name: 'Leifer'
+      id: '1',
+      name: 'Leifer',
     },
     {
-      id:'2',
-      name: 'Maria'
+      id: '2',
+      name: 'Maria',
     },
     {
-      id:'3',
-      name:'pedro'
-    }
-  ]
-
+      id: '3',
+      name: 'pedro',
+    },
+  ];
 }
